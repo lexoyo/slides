aa5150a94b1b842aa95a578a3f11ad1a731bf475
Alex Hoyau <a.hoyau@silexlabs.org>
2025-11-11 11:09:39 +0100

    link to editor in new tab

--- src/js/admin.js
+++ src/js/admin.js
@@ -218,7 +218,7 @@ class AdminDashboard {
     }
 
     editPresentation(id) {
-        window.location.href = `/editor?id=${id}`;
+        window.open(`/editor?id=${id}`, '_blank');
     }
 
     viewPresentation(id) {
